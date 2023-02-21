def53334bcb807590adaaae5fd4175ebd1c47603
Amuom254 <amuom.ke@gmail.com>
2023-02-21 05:30:15 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * main - Entry point
- * Description: prints 'Holberton \n'
- * Return: Always 0 (success)
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char str[] = "Holberton\n";
-int i = 0;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-while (str[i] != '\0')
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-	char c = str[i];
-
-	_putchar(c);
-	i++;
+		_putchar(str[count]);
 	}
-return (0);
+	_putchar('\n');
+	return (0);
 }
