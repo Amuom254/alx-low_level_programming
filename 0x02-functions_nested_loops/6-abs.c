6cedceef0fd927230a8f15a4445f9eb04594a955
Amuom254 <amuom.ke@gmail.com>
2023-02-21 06:57:28 -0800

    function that computes the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,24 +1,25 @@
-#include "holberton.h"
+#include "main.h"
+	#include <stdio.h>
 
-/**
- * _abs - Entry point
- * @n: n is an integer
- * Description: prints the absolute value of an integer
- * Return: int
- */
 
-int _abs(int n)
-{
-	if (n > 0)
+	/**
+	 * main - check the code
+	 *
+	 * Return: Always 0.
+	 */
+	int main(void)
 	{
-		return (n);
+	    int r;
+
+
+	    r = _abs(-1);
+	    printf("%d\n", r);
+	    r = _abs(0);
+	    printf("%d\n", r);
+	    r = _abs(1);
+	    printf("%d\n", r);
+	    r = _abs(-98);
+	    printf("%d\n", r);
+	    return (0);
 	}
-	else if (n < 0)
-	{
-		return (-n);
-	}
-	else
-	{
-		return (n);
-	}
-}
+
